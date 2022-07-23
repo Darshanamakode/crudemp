b3769e9b5f54fd7716de2e78f89d500f39a549f0
HP <HP@DESKTOP-H7S3SH5>
2022-07-23 12:36:54 +0530

    Add project files.

---
+++ HRMSProject.Repository/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HRMSProject.Repository.Models
+{
+    public partial class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+        public int DeptId { get; set; }
+
+        public virtual Department Dept { get; set; }
+    }
+}
